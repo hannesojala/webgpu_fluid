10c84207227227f681482f5e82de0d6c3afc01f6
Hannes Ojala <hannesojala@pop-os.localdomain>
2022-03-21 13:11:52 -0700

    Screen Quad

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use wgpu::{Backends, Device, DeviceDescriptor, Features, Instance, Limits, PowerPreference, Queue, RequestAdapterOptions, Surface, SurfaceConfiguration};
+use std::{slice, mem};
+
+use wgpu::{Backends, Device, DeviceDescriptor, Features, Instance, Limits, PowerPreference, Queue, RequestAdapterOptions, Surface, SurfaceConfiguration, PipelineLayoutDescriptor, ShaderModuleDescriptor, ShaderSource, VertexState, util::{DeviceExt, BufferInitDescriptor}, BufferSlice};
 use winit::{dpi::PhysicalSize, event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent}, event_loop::{ControlFlow, EventLoop}, window::Window};
 
 struct App {
@@ -7,7 +9,40 @@ struct App {
     queue:      Queue,
     config:     SurfaceConfiguration,
     size:       PhysicalSize<u32>,
+    render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
 }
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+struct Vertex {
+    position: [f32; 3],
+}
+
+impl Vertex {
+    const ATTRIBS: [wgpu::VertexAttribute; 2] =
+        wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3];
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: mem::size_of::<Self>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBS,
+        }
+    }
+}
+
+
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [1.0, 1.0, 0.0] },
+    Vertex { position: [-1., 1.0, 0.0] },
+    Vertex { position: [-1., -1., 0.0] },
+    Vertex { position: [1.0, -1., 0.0] },
+];
+
+const INDICES: &[u32] = &[
+    0, 1, 2, 0, 2, 3
+];
 
 impl App {
     pub fn new(window: &Window) -> Self {
@@ -32,9 +67,64 @@ impl App {
             height: size.height,
             present_mode: wgpu::PresentMode::Fifo,
         };
+        let shader = device.create_shader_module(&ShaderModuleDescriptor {
+            label: Some("ShaderModule"),
+            source: ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        });
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor { 
+            label: Some("Render Pipeline"),
+            layout: Some(&device.create_pipeline_layout(&PipelineLayoutDescriptor {
+                label: Some("Pipeline Layout Descriptor"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            })),
+            vertex: VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[Vertex::desc()],
+            },
+            fragment: Some(wgpu::FragmentState { // 3.
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[wgpu::ColorTargetState { // 4.
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                }],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw, // 2.
+                cull_mode: Some(wgpu::Face::Back),
+                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                polygon_mode: wgpu::PolygonMode::Fill,
+                // Requires Features::DEPTH_CLIP_CONTROL
+                unclipped_depth: false,
+                // Requires Features::CONSERVATIVE_RASTERIZATION
+                conservative: false,
+            },
+            depth_stencil: None, // 1.
+            multisample: wgpu::MultisampleState {
+                count: 1, // 2.
+                mask: !0, // 3.
+                alpha_to_coverage_enabled: false, // 4.
+            },
+            multiview: None, // 5.
+        });
         surface.configure(&device, &config);
+        let vertex_buffer = device.create_buffer_init(&BufferInitDescriptor{
+            label: Some("Vertex Buffer"),
+            contents: slice_to_u8_slice(VERTICES),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+        let index_buffer = device.create_buffer_init(&BufferInitDescriptor{
+            label: Some("Index Buffer"),
+            contents: slice_to_u8_slice(INDICES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
         App {
-            surface, device, queue, config, size
+            surface, device, queue, config, size, render_pipeline, vertex_buffer, index_buffer
         }
     }
     pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
@@ -44,33 +134,46 @@ impl App {
         self.surface.configure(&self.device, &self.config);
     }
     pub fn render(&mut self) {
-        let output = self.surface.get_current_texture().unwrap();
-        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+        // Get the next texture to draw to in swapchain
+        let swapchain_texture = self.surface
+            .get_current_texture()
+            .expect("Failed to obtain next swapchain image: ");
+        // Create a texture view of the output texture
+        let swapchain_texture_view = swapchain_texture.texture.create_view(&wgpu::TextureViewDescriptor::default());
+        // Encode commands: A render pass
         let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
             label: Some("Render Encoder"),
         });
+        // Create RenderPass
         {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
-                color_attachments: &[wgpu::RenderPassColorAttachment {
-                    view: &view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1, // Pick any color you want here
-                            g: 0.9,
-                            b: 0.3,
-                            a: 1.0,
-                        }),
-                        store: true,
-                    },
-                }],
-                depth_stencil_attachment: None,
+                color_attachments: &[
+                    wgpu::RenderPassColorAttachment {
+                        view: &swapchain_texture_view, // Texture view to use
+                        resolve_target: None, // for multisampling?
+                        // List of operations which will used
+                        ops: wgpu::Operations {
+                            // LoadOp: The op done on view at begining of the pass
+                            load: wgpu::LoadOp::Clear(wgpu::Color {
+                                r: 0.1, g: 0.9, b: 0.3, a: 1.0,
+                            }),
+                            store: true, // Whether data is written
+                        },
+                    }
+                ],
+                depth_stencil_attachment: None, // Depth attachment, if used
             });
+            // Set pipeline to use
+            render_pass.set_pipeline(&self.render_pipeline);
+            // Draw Command: Vertices and instances
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint32);
+            render_pass.draw_indexed(0..6, 0, 0..1);
         }
-        // submit will accept anything that implements IntoIter
+        // submit iterator of command buffers and present
         self.queue.submit(std::iter::once(encoder.finish()));
-        output.present();
+        swapchain_texture.present();
     }
 }
 
@@ -94,8 +197,17 @@ fn main() {
                 WindowEvent::Resized(size) => app.resize(*size),
                 _ => {}
             },
-            Event::RedrawRequested(window_id) if window_id == window.id() => app.render(),
+            Event::MainEventsCleared => {
+                app.render();
+            },
             _ => {}
         }
     });
+}
+
+// A bad idea
+fn slice_to_u8_slice<'a, T>(s: &'a [T]) -> &'a [u8] {
+    unsafe {
+        slice::from_raw_parts(s.as_ptr() as *const u8, mem::size_of::<T>()*s.len())
+    }
 }
